acff6e8a809b2110f2c90dc6411baa1c37a5a80f
Matthew Cosgrove <mc.matthew.cosgrove@gmail.com>
2020-10-05 13:18:06 +0100

    Update tag_version.sh

--- .github/scripts/tag_version.sh
+++ .github/scripts/tag_version.sh
@@ -7,12 +7,12 @@ set -euo pipefail
 env
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 galaxy_yaml_file="${SCRIPT_DIR}"/../../galaxy.yml
-echo "Current galaxy.yml version""
+echo "Current galaxy.yml version"
 cat ${galaxy_yaml_file} | grep version:
-tag_version=${GITHUB_REF#refs/tags/v}
-echo "Processing tag version v${tag_version}"
+sem_version="${GITHUB_REF#refs/tags/v}"
+echo "Processing tag version v${sem_version}"
   
-sed -i "/^version: /s/: .*$/: ${tag_version}/" "${galaxy_yaml_file}"
+sed -i "/^version: /s/: .*$/: ${sem_version}/" "${galaxy_yaml_file}"
 
 echo "Updated galaxy.yml version""
 cat "${galaxy_yaml_file}" | grep version:
